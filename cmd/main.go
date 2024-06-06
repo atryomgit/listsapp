4d4fc47918c663aff398a0fe1f2153fe1c9976c3
artyom <pestovartyomwork@gmail.com>
2024-06-06 10:40:23 +0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	todo "listsapp"
+	"listsapp/pack/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
-		log.Fatal("error occured")
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running server: %s", err.Error())
 	}
 }
